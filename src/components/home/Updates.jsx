1db0ce94c5b5db17cf3a63fce0ffa9c0da0220fb
Rob <ucw_feedback2002@yahoo.ca>
2023-09-02 14:29:52 -0400

    Updating updates.

--- src/components/home/Updates.jsx
+++ src/components/home/Updates.jsx
@@ -7,14 +7,33 @@ function Updates() {
     <Wrapper>
       <Title>LATEST UPDATES</Title>
       <P>
+        <strong>September 2nd, 2023</strong>
+      </P>
+      <P>
+        I&apos;ll be giving another Analog Video Glitch workshop alongside
+        Guillaume Vallée and Ganesh Baron Aloir this coming January at Eastern
+        Bloc. One day one Guillaume and I will be teaching the basics of video
+        glitch forms, from video feedback to VHS manipulation. We'll have an
+        assortment of circuit bent machinery for the class to use and
+        experiment, recording direct to tape. Day two Ganesh will be leading the
+        class on how to build their very own "dirty mixer." No circuit bending
+        or soldering experience required!
+      </P>
+      <StyledLink
+        to="https://www.eventbrite.ca/e/exploration-de-lart-video-a-travers-le-vhs-et-le-glitch-analogique-tickets-690986086987?aff=oddtdtcreator"
+        target="_blank"
+      >
+        <P>Sign up via Eastern Bloc</P>
+      </StyledLink>
+      <P>
         <strong>April 19th, 2023</strong>
       </P>
       <P>
         THE DIABETIC, a feature-length film by Mitchell Stafiej, is now
         available for free streaming on Tubi. I created visual effects seen
         during the opening segment, and collaborated with Mitchell for the
-        original score under the project name FUCKERS. It&apos;s a wild film, a must
-        for fans of The Comedy, Good Time, and lo-fi mayhem.
+        original score under the project name FUCKERS. It&apos;s a wild film, a
+        must for fans of The Comedy, Good Time, and lo-fi mayhem.
       </P>
       <StyledLink
         to="https://tubitv.com/movies/724073/the-diabetic"
